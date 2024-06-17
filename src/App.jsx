791567969dd3ef478609f59c02920c6a92f2632c
namish01 <namishbhargav121@gmail.com>
2024-06-18 01:25:57 +0530

    uploaded

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import DigitalClock from "./digitalClock"
+function App() {
+  return(
+    <>
+    <DigitalClock/>
+    </>
+  )
+}
+
+export default App
---
+++ src/digitalClock.jsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+
+function DigitalClock(){
+    const[time,setTime]=useState(new Date());
+    useEffect(()=>{
+        const intervalId=setInterval(()=>{
+            setTime(new Date());
+        },1000);
+        return ()=>{
+            clearInterval(intervalId);
+        }
+    },[])
+
+    function formatTime(){
+        let hours=time.getHours();
+        const Minutes=time.getMinutes();
+        const seconds=time.getSeconds();
+        const meridiam=hours>=12 ? "PM":"AM";
+
+        hours=hours%12||12;
+
+        return `${hours}:${Minutes}:${seconds} ${meridiam}`;
+    }
+    return(
+        <div className="clock-container">
+            <div className="clock">
+           <span>{formatTime()}</span>
+            </div>
+        </div>  
+          )
+}
+export default DigitalClock;
